fc3fb6f7031624e02e9b7e2cbebf7056394cca71
aantilao <a.antilaoortiz@gmail.com>
2021-01-30 20:05:49 -0300

    ADD: test

---
+++ test.js
@@ -0,0 +1,18 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+
+chai.use(chaiHttp);
+const url = "localhost:3000/fibonacci";
+describe('obtener n-esimo', () => {
+    it('El numero deberia ser', (done) => {
+        chai.request(url)
+        .get('/3')
+        .end(function(err,res) {
+            console.log(res.body)
+            expect(res).to.have.status(200);
+            done();
+        });
+    })
+});
