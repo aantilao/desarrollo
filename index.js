105b6a16d35651f3d86a1063eef112bf6e62a5bd
aantilao <a.antilaoortiz@gmail.com>
2021-01-30 18:47:33 -0300

    ADD: new route for fibbo

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/fibonacci/:number', function(req, res) {
+    console.log(req.params.number);
+    var a = 1;
+    b=0;
+    var temp;
+    var num = req.params.number;
+    if(Math.sign(num) == 1) {
+        var promise = new Promise ((resolve, reject) => {
+            while(num-1 > 0) {
+                temp= a;
+                a+= b;
+                b = temp;
+                num-= 1;
+                console.log(b);
+            }
+            res.send({number:b});
+        });
+    } else {
+        res.send('solo se aceptan numeros positivos sin incluis el 0');
+    }
+    
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
