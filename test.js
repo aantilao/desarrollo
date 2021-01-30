084249464ef6768995b65c91c3e13006d8d32ff4
aantilao <a.antilaoortiz@gmail.com>
2021-01-30 20:17:11 -0300

    ADD: more test

--- test.js
+++ test.js
@@ -16,3 +16,25 @@ describe('obtener n-esimo', () => {
         });
     })
 });
+describe('obtener n-esimo', () => {
+    it('El numero deberia ser', (done) => {
+        chai.request(url)
+        .get('/0')
+        .end(function(err,res) {
+            console.log(res.body)
+            expect(res).to.have.status(200);
+            done();
+        });
+    })
+});
+describe('obtener n-esimo', () => {
+    it('El numero deberia ser', (done) => {
+        chai.request(url)
+        .get('/sdaknldsakn')
+        .end(function(err,res) {
+            console.log(res.body)
+            expect(res).to.have.status(200);
+            done();
+        });
+    })
+});
